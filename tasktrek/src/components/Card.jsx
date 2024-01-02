959db9a8732aaa20d41ba1d1cc7b7f8fb7619c0e
angela <peekaboo32@naver.com>
2024-01-02 15:24:17 +0900

    컴포넌트 분리

--- tasktrek/src/components/Card.jsx
+++ tasktrek/src/components/Card.jsx
@@ -1,6 +1,13 @@
 import React from "react";
 
+// function Card(){
+//   return <h1>Card Component</h1>;
+// }
 
-function Card(){
-  return <h1>Card Component</h1>   
+const Card = () =>{
+    return (
+        <div>Card Component</div>
+    )
 }
+
+export default Card;
