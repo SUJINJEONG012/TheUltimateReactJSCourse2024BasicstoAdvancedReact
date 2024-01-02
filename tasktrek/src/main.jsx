0d187d5e1faf6db5e4380229a5b7192f50cea510
angela <peekaboo32@naver.com>
2024-01-02 15:38:54 +0900

    CreateTodo 모듈 분리하기

--- tasktrek/src/main.jsx
+++ tasktrek/src/main.jsx
@@ -3,9 +3,10 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import Card from './components/Card.jsx'
 import './index.css'
+import CreateTodo from './components/CreateTodo.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Card />
+    <CreateTodo />
   </React.StrictMode>,
 )
